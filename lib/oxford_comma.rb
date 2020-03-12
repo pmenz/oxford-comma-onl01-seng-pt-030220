095e10c08be3e5e2cca951f94b1d7e3d96f7617e
Pablo Menz <p.menz.r@gmail.com>
2020-03-12 01:49:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
  str = ""
  if array.count == 1 
     array[0]
+  end
+  
  array.each_with_index {| n , index | if index == array.count - 1 
  str << ", and " + n end }
  str
